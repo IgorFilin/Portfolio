9c1546ea736a19cf0c49d1e9aae2632915eaaaaa
Igor <filinigor@yandex.ru>
2022-10-01 00:01:16 +0300

    Commit

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -13,6 +13,14 @@ import gitSvg from './../common/img/icons/git.svg'
 import sassSvg from './../common/img/icons/sass.svg'
 import tddSvg from './../common/img/icons/tdd.svg'
 import storyBookSvg from './../common/img/icons/storyBook.svg'
+import formikSvg from './../common/img/icons/formik.svg'
+import materialUISvg from './../common/img/icons/materialUI.svg'
+import antDesignSvg from './../common/img/icons/antDesign.svg'
+import figmaSvg from './../common/img/icons/figma.svg'
+import webpackSvg from './../common/img/icons/webpack.svg'
+import nodeSvg from './../common/img/icons/node.svg'
+import unitTestsSvg from './../common/img/icons/unitTests.svg'
+import javascriptSvg from './../common/img/icons/javascript.svg'
 
 export const Skills = () => {
 
@@ -49,6 +57,30 @@ export const Skills = () => {
     const storyBook = {
         backgroundImage:`url(${storyBookSvg})`,
     }
+    const formik = {
+        backgroundImage:`url(${formikSvg})`,
+    }
+    const materialUI = {
+        backgroundImage:`url(${materialUISvg})`,
+    }
+    const antDesign = {
+        backgroundImage:`url(${antDesignSvg})`,
+    }
+    const figma = {
+        backgroundImage:`url(${figmaSvg})`,
+    }
+    const webpack = {
+        backgroundImage:`url(${webpackSvg})`,
+    }
+    const node = {
+        backgroundImage:`url(${nodeSvg})`,
+    }
+    const unitTests = {
+        backgroundImage:`url(${unitTestsSvg})`,
+    }
+    const javascript = {
+        backgroundImage:`url(${javascriptSvg})`,
+    }
     return (
         <div className={s.mainBlock}>
             <div className={s.container}>
@@ -57,6 +89,7 @@ export const Skills = () => {
                     <Skill style={react} title={'REACT'} discription={'discription skill'}/>
                     <Skill style={redux} title={'REDUX'} discription={'discription skill'}/>
                     <Skill style={typeScript} title={'TYPESCRIPT'}discription={'discription skill'}/>
+                    <Skill style={javascript} title={'JAVASCRIPT'}discription={'discription skill'}/>
                     <Skill style={html} title={'HTML'}discription={'discription skill'}/>
                     <Skill style={css} title={'CSS'}discription={'discription skill'}/>
                     <Skill style={api} title={'REST API'}discription={'discription skill'}/>
@@ -65,14 +98,13 @@ export const Skills = () => {
                     <Skill style={sass} title={'SASS/SCSS'}discription={'discription skill'}/>
                     <Skill style={tdd} title={'TDD'}discription={'discription skill'}/>
                     <Skill style={storyBook} title={'STORYBOOK'}discription={'discription skill'}/>
-                    <Skill title={'FORMIK'}discription={'discription skill'}/>
-                    <Skill title={'MATERIAL-UI'}discription={'discription skill'}/>
-                    <Skill title={'ANT-DESIGN'}discription={'discription skill'}/>
-                    <Skill title={'FIGMA'}discription={'discription skill'}/>
-                    <Skill title={'WEBPACK'}discription={'discription skill'}/>
-                    <Skill title={'NODE-JS'}discription={'discription skill'}/>
-                    <Skill title={'UNIT-TESTING'}discription={'discription skill'}/>
-                    <Skill title={'JAVASCRIPT'}discription={'discription skill'}/>
+                    <Skill style={formik} title={'FORMIK'}discription={'discription skill'}/>
+                    <Skill style={materialUI} title={'MATERIAL-UI'}discription={'discription skill'}/>
+                    <Skill style={antDesign} title={'ANT-DESIGN'}discription={'discription skill'}/>
+                    <Skill style={figma} title={'FIGMA'}discription={'discription skill'}/>
+                    <Skill style={webpack} title={'WEBPACK'}discription={'discription skill'}/>
+                    <Skill style={node}  title={'NODE-JS'}discription={'discription skill'}/>
+                    <Skill  style={unitTests} title={'UNIT-TESTING'}discription={'discription skill'}/>
                 </div>
             </div>
         </div>
